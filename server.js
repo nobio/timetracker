acb73537fa8ec6a2eba98bfd335300a6f4417a96
Gernot H. Reichel <gernot.reichel@me.com>
2016-11-19 12:31:12 +0100

    using cron like method to schedule tasts (dump, backup, calcstats)

--- server.js
+++ server.js
@@ -86,14 +86,37 @@ http.createServer(app).listen(app.get('port'), app.get('host'), function() {
 
 // start the scheduler
 console.log("job scheduler: calcStats");
-schedule.scheduleJob({minute: 0}, function() {
+//schedule.scheduleJob({minute: 0}, function() {
+schedule.scheduleJob('0 * * * *', function() {  // every hour at ??:00
+	console.log('scheduled task "calcStats" started')
     admin.calcStats;
 });
 console.log("job scheduler: dumpTimeEntry");
-schedule.scheduleJob({hour: 4, minute: 0}, function() {
+//schedule.scheduleJob({hour: 4, minute: 0}, function() {
+schedule.scheduleJob('5 4 * * *', function() {  // every day at 04:05
+	console.log('scheduled task "dumpTimeEntry" started')
     admin.dumpTimeEntry;
 });
 console.log("job scheduler: backupTimeEntry");
-schedule.scheduleJob({hour: 4, minute: 0}, function() {
+//schedule.scheduleJob({hour: 4, minute: 0}, function() {
+//schedule.scheduleJob('10 4 * * *', function() {  // every day at 04:10
+schedule.scheduleJob('* * * * *', function() {  // every day at 04:10
+	console.log('scheduled task "bacupTimeEntry" started')
     admin.backupTimeEntry;
 });
+console.log("job scheduler: test");
+schedule.scheduleJob('* * * * *', function() {
+    console.log('schedule: ' + new Date());
+}); 
+
+
+/*
+# .---------------- minute (0 - 59)
+# |  .------------- hour (0 - 23)
+# |  |  .---------- day of month (1 - 31)
+# |  |  |  .------- month (1 - 12) OR jan,feb,mar,apr ...
+# |  |  |  |  .---- day of week (0 - 6) (Sunday=0 or 7) OR sun,mon,tue,wed,thu,fri,sat
+# |  |  |  |  |
+# *  *  *  *  * user-name  command to be executed
+schedule.scheduleJob('0 17 ? * 0,4-6', function(){
+*/
