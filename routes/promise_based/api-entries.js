0b64b127ed139b248d8cb168a5a4cd8e5a34392a
Gernot H. Reichel <gernot.reichel@me.com>
2017-12-03 15:18:55 +0100

    implementing CRUD of Time Entry https://github.com/nobio/timetrack/issues/7

--- routes/promise_based/api-entries.js
+++ routes/promise_based/api-entries.js
@@ -3,18 +3,64 @@ var moment = require('moment')
 var tz = require('moment-timezone')
 var util = require('./util')
 var TimeEntry = mongoose.model('TimeEntry')
+const DEFAULT_BREAK_TIME = 45 * 60 * 1000 // 45 min in milli seconds
 
-/*
+/********************************************************************************
+ * Get one Time Entry by it's id
+ * 
+ * curl -X GET http://localhost:30000/entries/5a2100cf87f1f368d087696a
+ *******************************************************************************/
+exports.getEntryById = (req, res) => {
+  TimeEntry.findById(req.params.id)
+    .then(timeentry => res.send(timeentry))
+    .catch(err => res.send(500, 'Error while reading Time Entry: ' + req.params.id + ' ' + err))
+}
+
+/********************************************************************************
+ * Reads all time entries
+ * Read entries by date: getAllByDate
+ * Read busy time: getBusyTime
+ *
+ * curl -X GET http://localhost:30000/api/entries
+ * curl -X GET http://localhost:30000/api/entries?dt=1393455600000
+ * curl -X GET http://localhost:30000/api/entries?busy=1393455600000
+ *******************************************************************************/
+exports.getEntries = (req, res) => {
+  var filterByDate = req.query.dt
+  var filterByBusy = req.query.busy
+
+  if (filterByDate && filterByBusy) {
+    console.log('filter by date and busy')
+    res.send(500, 'date and busy filter set; can only handle one of them')
+  } else if (filterByDate) {
+    console.log('filter by date: ' + filterByDate)
+    getAllByDate(filterByDate)
+      .then(timeentries => res.send(timeentries))
+      .catch(err => res.send(500, err))
+  } else if (filterByBusy) {
+    console.log('filter by busy: ' + filterByBusy)
+    getAllByDate(filterByBusy)
+      .then(getBusyTime)
+      .then(busytime => res.send({'duration': busytime}))
+      .catch(err => res.send(500, err))
+  } else {
+    TimeEntry.find()
+      .then(timeentry => res.send(timeentry))
+      .catch(err => res.send(500, 'Error while reading Time Entry: ' + req.params.id + ' ' + err))
+  }
+}
+
+/********************************************************************************
  * Creates a new TimeEntry value. Input data:
- * - direction (enter/go)
- * - entry_date
- * - last_changed
- * - date_time
- * - longitude (optional)
- * - latitude (optional)
+ * @param direction (enter/go)
+ * @param entry_date
+ * @param last_changed
+ * @param date_time
+ * @param longitude (optional)
+ * @param latitude (optional)
  * 
  * curl -X POST -H "Content-Type: application/json" -d '{"direction":"enter","entry_date":"2017-12-02T17:49:23.977Z","last_changed":"2017-12-02T17:49:23.977Z","datetime":"2017-12-02T17:49:00.000Z"}' http://localhost:30000/api/entries
- */
+ *******************************************************************************/
 exports.createEntry = (req, res) => {
   console.log(JSON.stringify(req.body))
   var direction = req.body.direction
@@ -36,56 +82,90 @@ exports.createEntry = (req, res) => {
     }
   })
 }
-/*
+
+/********************************************************************************
  * deletes one time entry by it's id
- */
+ * 
+ * curl -X DELETE http://localhost:30000/api/entries/5a24076af89b40156b1c0efe
+ *******************************************************************************/
 exports.deleteEntry = (req, res) => {
   var id = req.params.id
 
-  TimeEntry.findByIdAndRemove(id, (err) => {
-    if (err) {
-      res.send(500, 'Error while deleting Time Entry: ' + id + ' ' + err.message)
-    } else {
-      res.send(id)
-    }
-  })
+  TimeEntry.findByIdAndRemove(id)
+    .then(timeentry => res.send(timeentry))
+    .catch(err => res.send(500, 'Error while reading Time Entry: ' + req.params.id + ' ' + err))
 }
 
-/*
- * Reads all time entries
- * Read entries by date: getAllByDate
- * Read busy time: getBusyTime
- *
- * curl -X GET http://localhost:30000/entries
- * curl -X GET http://localhost:30000/entries?dt=1393455600000
- * curl -X GET http://localhost:30000/entries?busy=1393455600000
- */
-exports.getEntries = (req, res) => {
-}
-
-/*
- * lists all Time Entries for a given date (this particular day)
- *
- * curl -X GET http://localhost:30000/entries/dt/1451084400000
- */
-function getAllByDate (date, res) {
-}
-
-/*
- * get one Time Entry by it's id
- */
-exports.getEntryById = (req, res) => {
-
-  TimeEntry.findById(req.params.id, (err, timeentry) => {
-    if (err) {
-      res.send(500, 'Error while reading Time Entry: ' + req.params.id + ' ' + err)
-    } else {
-      res.send(timeentry)
-    }
-  })
-}
-/*
+/********************************************************************************
  * stores one Time Entry
- */
+ *******************************************************************************/
 exports.storeEntryById = (req, res) => {
+}
+
+/* =========================================================================== */
+/* =========================================================================== */
+/*                              PRIVATE METHODS                                */
+/* =========================================================================== */
+/* =========================================================================== */
+
+/**
+ * lists all Time Entries for a given date (this particular day)
+ * 
+ * @returns Promise
+ */
+function getAllByDate (date) {
+  var dtStart = util.stripdownToDateBerlin(moment.unix(date / 1000))
+  var dtEnd = moment(dtStart).add(1, 'days')
+  // console.log('getAllByDate received date:               ' + moment(dt).format('DD.MM.YYYY HH:mm:ss'))
+
+  return new Promise((resolve, reject) => {
+    TimeEntry.find({
+      entry_date: {
+        $gte: dtStart,
+        $lt: dtEnd
+      }
+    }).skip(0).sort({
+      entry_date: 1
+    })
+      .then(timeentries => resolve(timeentries))
+      .catch(err => reject(new Error('Unable to read Time Entry for given date : ' + date + ' (' + err.message + ')')))
+  })
+}
+
+/**
+ * Reads the busy time of all entries for a given day
+ * 
+ * @returns Promise
+ */
+function getBusyTime (timeentries) {
+  return new Promise((resolve, reject) => {
+
+    if (timeentries.length === 0) {
+      reject(new Error('Es gibt keine Einträge für diesen Tag (' + dt.format('DD.MM.YYYY') + ')'), 0)
+    } else if (timeentries.length % 2 !== 0) {
+      reject(new Error('Bitte die Einträge für diesen Tag (' + dt.format('DD.MM.YYYY') + ') vervollständigen'), 0)
+    } else {
+      var busytime = 0
+      for (var n = timeentries.length - 1; n > 0; n -= 2) {
+        // this must be a go-event
+        if (timeentries[n].direction !== 'go') {
+          reject(new Error('Die Reihenfolge der Kommen/Gehen-Einträge am ' + dt.format('DD.MM.YYYY') + ' scheint nicht zu stimmen.'), 0)
+          return
+        }
+
+        var end = timeentries[n].entry_date
+        var start = timeentries[n - 1].entry_date
+
+        busytime = busytime + (end - start)
+      }
+
+      // when there have been only 2 entries we reduce the busytime by 45 minutes (default pause)
+      if (timeentries.length === 2) {
+        busytime = busytime - DEFAULT_BREAK_TIME
+      }
+      // console.log(dt + " => " + busytime + " " + (busytime/1000/60/60))
+
+      resolve(busytime)
+    }
+  })
 }
--- routes/promise_based/util.js
+++ routes/promise_based/util.js
@@ -1,4 +1,24 @@
+/**
+ * 
+ * Methods of this file must never be used from server.js directly rather than from api layer
+ * 
+ */
+
 require('../../db/db')
 var mongoose = require('mongoose')
 var moment = require('moment')
 var TimeEntry = mongoose.model('TimeEntry')
+
+/**
+ * takes the date and removes all time components
+ * date expected to be a moment object
+ */
+exports.stripdownToDateBerlin = (date) => {
+  var d = moment.tz(date / 1, 'Europe/Berlin')
+  d.millisecond(0)
+  d.second(0)
+  d.minutes(0)
+  d.hours(0)
+
+  return d
+}
