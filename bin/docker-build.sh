9beb22b39191f2e54f93177c784c67dce24d3331
Gernot H. Reichel <gernot.reichel@me.com>
2018-06-01 14:16:24 +0200

    pimping my docker stuff

--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -3,8 +3,8 @@ TARGETDIR=/share/CACHEDEV1_DATA/homes/admin/Projects/timetracker-docker
 export PATH=$PATH:/share/CACHEDEV1_DATA/.qpkg/container-station/bin
 cd $TARGETDIR
 
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+#docker stop $(docker ps -a -q)
+docker stop timetracker
+docker rm timetracker
 docker build -t nobio/timetracker .   
-docker run --restart=always -p 30030:30000 -d nobio/timetracker
-
+docker run --restart=always -p 30030:30000 --name timetracker -d nobio/timetracker
