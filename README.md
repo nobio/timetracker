089b77687fafbe7b3cbd5b8ae5dcfda445d68dcd
Gernot H. Reichel <gernot.reichel@me.com>
2018-06-02 21:19:36 +0200

    docker stuff

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ TimeTracker
 ===========
 
 tracks time of my adsence
+
+## Deployment
+To deploy in my qnap NAS please use the following command (root directory)
+
+`$ bin/rsync-qnap.sh`
--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -5,6 +5,7 @@ cd $TARGETDIR
 
 #docker stop $(docker ps -a -q)
 docker stop timetracker
+#docker rm $(docker ps -a -q)
 docker rm timetracker
 docker build -t nobio/timetracker .   
 docker run --restart=always -p 30030:30000 --name timetracker -d nobio/timetracker
