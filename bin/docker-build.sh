218040542f93314233860bcd312d19bf9c8d9108
Gernot H. Reichel <gernot.reichel@me.com>
2018-06-02 21:30:12 +0200

    corrected conflicts

--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -8,9 +8,4 @@ docker stop timetracker
 #docker rm $(docker ps -a -q)
 docker rm timetracker
 docker build -t nobio/timetracker .   
-<<<<<<< HEAD
-docker run -rm --restart=always -p 30030:30000 -d nobio/timetracker
-
-=======
-docker run --restart=always -p 30030:30000 --name timetracker -d nobio/timetracker
->>>>>>> 6bd532270da6d1915d03d65aaf5513bc5f2cf92e
+docker run -rm --restart=always -p 30030:30000 --name timetracker -d nobio/timetracker
