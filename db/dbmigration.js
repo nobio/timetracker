5fa992a493f13036091aad2251f2dcea7f5949ef
Gernot H. Reichel <gernot.reichel@me.com>
2018-07-08 18:29:12 +0200

    implementd data migration

--- db/dbmigration.js
+++ db/dbmigration.js
@@ -16,61 +16,92 @@ const migrationData = [];
 // Schema
 const directions = 'enter go'.split(' ');
 const TimeEntry = new schema({
-  entry_date: {
-    type: Date, required: true, default: Date.now, index: true,
-  },
-  direction: { type: String, enum: directions, required: true },
-  last_changed: { type: Date, default: Date.now, required: true },
-  longitude: { type: Number, required: false },
-  latitude: { type: Number, required: false },
+    entry_date: {
+        type: Date, required: true, default: Date.now, index: true,
+    },
+    direction: { type: String, enum: directions, required: true },
+    last_changed: { type: Date, default: Date.now, required: true },
+    longitude: { type: Number, required: false },
+    latitude: { type: Number, required: false },
 });
 
 const TIME_ENTRY_MODEL = mongoose.model('TimeEntry', TimeEntry);
+const TIME_ENTRY_MODEL_TARGET = mongoose.model('TimeEntry', TimeEntry);
 
 /**
- * Reads data from source data source and returns an json array 
+ * Reads data from source data source and returns an json array
  */
 function getDataFromSource() {
-  return new Promise((resolve, reject) => {
-    mongoose.connect(MONGO_URL_SOURCE, { useNewUrlParser: true });
+    return new Promise((resolve, reject) => {
+        mongoose.connect(MONGO_URL_SOURCE, { useNewUrlParser: true });
 
-    TIME_ENTRY_MODEL.find()
-      .then((timeEntries) => {
-        mongoose.connection.close();
-        resolve(timeEntries);
-      })
-      .catch((err) => {
-        mongoose.connection.close();
-        reject(err);
-      });
-  });
+        TIME_ENTRY_MODEL.find()
+            .then((timeEntries) => {
+                mongoose.connection.close();
+                resolve(timeEntries);
+            })
+            .catch((err) => {
+                mongoose.connection.close();
+                reject(err);
+            });
+    });
+}
+
+function deleteAllTarget() {
+    mongoose.connect(MONGO_URL_TARGET, { useNewUrlParser: true });
+
+    return new Promise((resolve, reject) => {
+        TIME_ENTRY_MODEL_TARGET.remove()
+            .then(() => resolve('ok'))
+            .catch((err) => reject(err));
+    });
 }
 
 /**
  * Stores the data to target data srouce read from source data source
- *  
- * @param {timeEntries} timeEntries 
+ *
+ * @param {timeEntries} timeEntries
  */
 function storeDataToTarget(timeEntries) {
-  return new Promise((resolve, reject) => {
+    let n = 0;
     mongoose.connect(MONGO_URL_TARGET, { useNewUrlParser: true });
-    TIME_ENTRY_MODEL.find()
-    .then((timeEntries) => {
-        mongoose.connection.close();
-        resolve(timeEntries.length);
-      })
-      .catch((err) => {
-        mongoose.connection.close();
-        reject(err);
-      });
-  });
+    return new Promise((resolve, reject) => {
+
+        timeEntries.forEach(timeentry => {
+
+            process.stdout.write('.');
+            new TIME_ENTRY_MODEL_TARGET({
+                _id: timeentry._id,
+                entry_date: timeentry.entry_date,
+                direction: timeentry.direction,
+                last_changed: timeentry.last_changed,
+                longitude: timeentry.longitude,
+                latitude: timeentry.latitude,
+            }).save()
+                .then(doc => {
+                    n++;
+                    if (n >= timeEntries.length) { mongoose.connection.close(); }
+                })
+                .catch(err => {
+                    n++;
+                    console.log(err.message);
+                    if (n >= timeEntries.length) { mongoose.connection.close(); }
+                    //reject(err);
+                });
+        });
+
+        resolve(`${timeEntries.length} elements saved`);
+        //mongoose.connection.close();
+
+    });
 }
 
 // start the migration...
 getDataFromSource()
-  .then(storeDataToTarget)
-  .then(l => console.log(l))
-  .catch(err => {
-    console.log(err);
-    process.exit(-1)
-  });
+    .then(storeDataToTarget)
+    .then(msg => console.log(msg))
+    .catch((err) => {
+        console.log(err);
+        console.log('***********************************************************************')
+        process.exit(-1);
+    })
