0f0a117fbfb3a8f7c8aba6a3c86f447c80c3ac11
Gernot H. Reichel <gernot.reichel@me.com>
2018-07-08 16:01:52 +0200

    implementing migration algorithm

--- db/dbmigration.js
+++ db/dbmigration.js
@@ -1,45 +1,76 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema;
+
 const MONGO_URL_MLAB = 'mongodb://nobio:1gR7hW2cPhtkRlv2@ds061928.mlab.com:61928/timetrack';
 const MONGO_URL_DOCKER = 'mongodb://qnap-nas:27017/timetracker';
 
 const MONGO_URL_SOURCE = MONGO_URL_MLAB;
 const MONGO_URL_TARGET = MONGO_URL_DOCKER;
 
-let migrationData = [];
+
+const migrationData = [];
 
 /* ==================================================================== */
-
-const mongoose = require('mongoose');
-const schema = mongoose.Schema;
-mongoose.Promise = global.Promise;
-
-
 
 // Schema
 const directions = 'enter go'.split(' ');
 const TimeEntry = new schema({
- entry_date: { type: Date, required: true, default: Date.now, index: true },
- direction: { type: String, enum: directions, required: true },
- last_changed: { type: Date, default: Date.now, required: true },
- longitude: { type: Number, required: false },
- latitude: { type: Number, required: false },
+  entry_date: {
+    type: Date, required: true, default: Date.now, index: true,
+  },
+  direction: { type: String, enum: directions, required: true },
+  last_changed: { type: Date, default: Date.now, required: true },
+  longitude: { type: Number, required: false },
+  latitude: { type: Number, required: false },
 });
-const timeEntry = mongoose.model('TimeEntry', TimeEntry);
 
+const TIME_ENTRY_MODEL = mongoose.model('TimeEntry', TimeEntry);
 
-console.log('connecting to ' + MONGO_URL_SOURCE + '...');
-mongoose.connect(MONGO_URL_SOURCE, { useNewUrlParser: true }).then(
- () => {
-  console.log('...connected');
-  return;
- },
- (err) => {
-  console.log(`error while connecting mongodb:${err}`);
- },
-);
+/**
+ * Reads data from source data source and returns an json array 
+ */
+function getDataFromSource() {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(MONGO_URL_SOURCE, { useNewUrlParser: true });
 
-timeEntry.find()
- .then(timeentries => migrationData = timeentries)
- .catch(err => console.log(err.message));
+    TIME_ENTRY_MODEL.find()
+      .then((timeEntries) => {
+        mongoose.connection.close();
+        resolve(timeEntries);
+      })
+      .catch((err) => {
+        mongoose.connection.close();
+        reject(err);
+      });
+  });
+}
 
+/**
+ * Stores the data to target data srouce read from source data source
+ *  
+ * @param {timeEntries} timeEntries 
+ */
+function storeDataToTarget(timeEntries) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(MONGO_URL_TARGET, { useNewUrlParser: true });
+    TIME_ENTRY_MODEL.find()
+    .then((timeEntries) => {
+        mongoose.connection.close();
+        resolve(timeEntries.length);
+      })
+      .catch((err) => {
+        mongoose.connection.close();
+        reject(err);
+      });
+  });
+}
 
-console.log(migrationData.length)
+// start the migration...
+getDataFromSource()
+  .then(storeDataToTarget)
+  .then(l => console.log(l))
+  .catch(err => {
+    console.log(err);
+    process.exit(-1)
+  });
