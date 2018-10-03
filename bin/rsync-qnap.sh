6bd2ac693ec4f500ca14d58535b5b2f81f646fb0
Gernot H. Reichel <gernot.reichel@me.com>
2018-10-03 19:27:47 +0200

    adapted docker stuff to get rid of source code on qnap

--- bin/rsync-qnap.sh
+++ bin/rsync-qnap.sh
@@ -5,8 +5,10 @@ QNAP_HOST=admin@qnap-nas
 #QNAP_HOST=admin@`ping -c 1 qnap-nas|grep "bytes from"|awk '{print $4}'|awk -F ":" '{print $1}'`
 #QNAP_HOST=admin@nobio.myhome-server.de
 WORKDIR=./
-TARGETDIR=/share/CACHEDEV1_DATA/homes/admin/Projects/timetracker-docker
+##TARGETDIR=/share/CACHEDEV1_DATA/homes/admin/Projects/timetracker-docker
+TARGETDIR=/share/CACHEDEV1_DATA/homes/admin/Projects/timetracker-composition
 
-rsync -avp --delete $WORKDIR $QNAP_HOST:$TARGETDIR --exclude node_modules --exclude dump --exclude data --exclude .git
+##rsync -avp --delete $WORKDIR $QNAP_HOST:$TARGETDIR --exclude node_modules --exclude dump --exclude data --exclude .git
+rsync -avp --delete "Dockerfile" "docker-compose.yml" $QNAP_HOST:$TARGETDIR 
 #ssh -t $QNAP_HOST $TARGETDIR/bin/docker-build.sh
 echo ...done
