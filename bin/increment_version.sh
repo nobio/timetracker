c7997edf907fbeae66baf554952aaea4ac112ce1
Gernot H. Reichel <gernot.reichel@me.com>
2019-01-13 14:26:02 +0100

    adapted howto increment build number

--- bin/increment_version.sh
+++
@@ -1,6 +0,0 @@
-export VERSION_FILE=./VERSION
-oldnum=`cut -d '.' -f3 $VERSION_FILE` # minor number number <ver.maj.min>
-newnum=`expr $oldnum + 1`
-version=`cat $VERSION_FILE | awk -F"." '{print $1}'`.`cat $VERSION_FILE | awk -F"." '{print $2}'`.$newnum
-echo $version > $VERSION_FILE
-echo new version number is $version
