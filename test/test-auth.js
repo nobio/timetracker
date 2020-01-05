68ec8db54712536dd363a9d86a7c0907c23fc87d
Gernot H. Reichel <gernot.reichel@me.com>
2020-01-05 12:19:49 +0100

    replaced test user and password by constants

--- test/test-auth.js
+++ test/test-auth.js
@@ -13,6 +13,9 @@ const should = chai.should;
 
 const util = require('../api/auth/util-auth');
 
+const TESTUSER_NAME = 'TEST_USER_DELETE_ME';
+const TESTUSER_PASSWORD = 'Test12345';
+
 describe('test utilAuth.getAllUsers', () => {
   let db;
   before(() => {
@@ -36,13 +39,13 @@ describe('test utilAuth.getAllUsers', () => {
 describe('test utilAuth.createUser', () => {
   let db;
   before(async () => {
-    await User.deleteOne({ name: 'TEST_USER_DELETE_ME' });
+    await User.deleteOne({ name: TESTUSER_NAME });
     db = require('../db');
   });
 
   it('create new user', async () => {
     try {
-      const result = await util.createUser('TEST_USER_DELETE_ME', '12345');
+      const result = await util.createUser(TESTUSER_NAME, TESTUSER_PASSWORD);
       expect(result).to.be.a('string');
       expect(result).to.equal('User created');
     } catch (err) {
@@ -52,7 +55,7 @@ describe('test utilAuth.createUser', () => {
 
   it('try to create user twice', async () => {
     try {
-      const result = await util.createUser('TEST_USER_DELETE_ME', '12345');
+      const result = await util.createUser(TESTUSER_NAME, TESTUSER_PASSWORD);
       assert.fail(); // should not reach this...
     } catch (err) {
       expect(err).to.be.an('error');
@@ -62,8 +65,8 @@ describe('test utilAuth.createUser', () => {
 
   it('try to create user without password (empty)', async () => {
     try {
-      await User.deleteOne({ name: 'TEST_USER_DELETE_ME' });
-      const result = await util.createUser('TEST_USER_DELETE_ME', '');
+      await User.deleteOne({ name: TESTUSER_NAME });
+      const result = await util.createUser(TESTUSER_NAME, '');
       assert.fail(); // should not reach this...
     } catch (err) {
       expect(err).to.be.an('error');
@@ -72,8 +75,8 @@ describe('test utilAuth.createUser', () => {
   });
   it('try to create user without password (undefined)', async () => {
     try {
-      await User.deleteOne({ name: 'TEST_USER_DELETE_ME' });
-      const result = await util.createUser('TEST_USER_DELETE_ME');
+      await User.deleteOne({ name: TESTUSER_NAME });
+      const result = await util.createUser(TESTUSER_NAME);
       assert.fail(); // should not reach this...
     } catch (err) {
       expect(err).to.be.an('error');
@@ -82,8 +85,8 @@ describe('test utilAuth.createUser', () => {
   });
   it('try to create user without password (null)', async () => {
     try {
-      await User.deleteOne({ name: 'TEST_USER_DELETE_ME' });
-      const result = await util.createUser('TEST_USER_DELETE_ME', null);
+      await User.deleteOne({ name: TESTUSER_NAME });
+      const result = await util.createUser(TESTUSER_NAME, null);
       assert.fail(); // should not reach this...
     } catch (err) {
       expect(err).to.be.an('error');
@@ -93,7 +96,7 @@ describe('test utilAuth.createUser', () => {
 
   it('try to create user without name (empty)', async () => {
     try {
-      await User.deleteOne({ name: 'TEST_USER_DELETE_ME' });
+      await User.deleteOne({ name: TESTUSER_NAME });
       const result = await util.createUser('', '');
       assert.fail(); // should not reach this...
     } catch (err) {
@@ -103,7 +106,7 @@ describe('test utilAuth.createUser', () => {
   });
   it('try to create user without name (undefined)', async () => {
     try {
-      await User.deleteOne({ name: 'TEST_USER_DELETE_ME' });
+      await User.deleteOne({ name: TESTUSER_NAME });
       const result = await util.createUser();
       assert.fail(); // should not reach this...
     } catch (err) {
@@ -113,7 +116,7 @@ describe('test utilAuth.createUser', () => {
   });
   it('try to create user without name (null)', async () => {
     try {
-      await User.deleteOne({ name: 'TEST_USER_DELETE_ME' });
+      await User.deleteOne({ name: TESTUSER_NAME });
       const result = await util.createUser(null, null);
       assert.fail(); // should not reach this...
     } catch (err) {
@@ -130,10 +133,10 @@ describe('test utilAuth.createUser', () => {
 
     it('login with valid principal and credenital', async () => {
       try {
-        await util.createUser('TEST_USER_DELETE_ME', '12345');
+        await util.createUser(TESTUSER_NAME, TESTUSER_PASSWORD);
         await new Promise(resolve => setTimeout(resolve, 100)); // sleep a little while...
 
-        const result = await util.login('TEST_USER_DELETE_ME', '12345');
+        const result = await util.login(TESTUSER_NAME, TESTUSER_PASSWORD);
 
         expect(result).to.be.a('string');
         expect(result).to.equal('User authenticated');
@@ -141,16 +144,16 @@ describe('test utilAuth.createUser', () => {
         throw Error(err);
       } finally {
         await new Promise(resolve => setTimeout(resolve, 100)); // sleep a little while...
-        await User.deleteOne({ name: 'TEST_USER_DELETE_ME' });
+        await User.deleteOne({ name: TESTUSER_NAME });
       }
     });
 
     it('login with valid principal and invalid credenital', async () => {
       try {
-        await util.createUser('TEST_USER_DELETE_ME', 'XXXXX');
+        await util.createUser(TESTUSER_NAME, 'XXXXX');
         await new Promise(resolve => setTimeout(resolve, 100)); // sleep a little while...
 
-        const result = await util.login('TEST_USER_DELETE_ME', '12345');
+        const result = await util.login(TESTUSER_NAME, TESTUSER_PASSWORD);
 
         assert.fail(); // should not reach this...
       } catch (err) {
@@ -158,7 +161,7 @@ describe('test utilAuth.createUser', () => {
         expect(err.message).to.equal('User not authenticated');
       } finally {
         await new Promise(resolve => setTimeout(resolve, 100)); // sleep a little while...
-        await User.deleteOne({ name: 'TEST_USER_DELETE_ME' });
+        await User.deleteOne({ name: TESTUSER_NAME });
       }
     });
     it('login with invalid principal and invalid credenital', async () => {
@@ -173,7 +176,7 @@ describe('test utilAuth.createUser', () => {
   });
 
   after(async () => {
-    await User.deleteOne({ name: 'TEST_USER_DELETE_ME' });
+    await User.deleteOne({ name: TESTUSER_NAME });
     db.closeConnection();
   });
 });
