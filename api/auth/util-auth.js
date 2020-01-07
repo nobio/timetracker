142548d5f3434551e22a9e88d755a726470d1bb9
Gernot Reichel <gernot.reichel@zeiss.com>
2020-01-07 10:41:12 +0100

    added comments and refactored

--- api/auth/util-auth.js
+++ api/auth/util-auth.js
@@ -1,16 +1,26 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-
 const User = mongoose.model('User');
 
+/**
+ * Reads all registeredusers
+ * 
+ * @returns Array of all registered users
+ * @param {*} name User name of user
+ * @param {*} password Password of user
+ */
 exports.getAllUsers = async () => User.find();
 
 /**
  * creates a new user in database
+ * 
+ * @param {*} name User name of user
+ * @param {*} password Password of user
  */
 exports.createUser = async (name, password) => {
   if (!name) throw Error('User must be provided');
   if (!password) throw Error('No password provided');
+
   const user = await User.findOne({ name });
   if (user) throw Error('User already exists');
 
@@ -23,16 +33,21 @@ exports.createUser = async (name, password) => {
   return 'User created';
 };
 
-/** login with user and password (needs to be passed) */
+/**
+ * login with user and password (needs to be passed) 
+ * 
+ * @param {*} name User name of user
+ * @param {*} password Password of user
+ */
 exports.login = async (name, password) => {
   if (!name) throw Error('User must be provided');
   if (!password) throw Error('No password provided');
 
   const user = await User.findOne({ name });
-
   if (user == null) throw Error('User not authenticated');
-  if (await bcrypt.compare(password, user.password)) {
-    return 'User authenticated';
+
+  if (!await bcrypt.compare(password, user.password)) {
+    throw Error('User not authenticated');
   }
-  throw Error('User not authenticated');
+  return 'User authenticated';
 };
