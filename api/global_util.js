fde85a34e67cc6b48e08b5ec9ed310c1ab243e19
Gernot H. Reichel <gernot.reichel@me.com>
2021-09-16 16:08:10 +0200

    changed default break time to 30 min.

--- api/global_util.js
+++ api/global_util.js
@@ -2,7 +2,7 @@ const moment = require('moment');
 require('moment-timezone');
 const toggleUtil = require('./admin/util-toggles');
 
-exports.DEFAULT_BREAK_TIME_SECONDS = 45 * 60;
+exports.DEFAULT_BREAK_TIME_SECONDS = 30 * 60;
 exports.DEFAULT_BREAK_TIME_MILLISECONDS = this.DEFAULT_BREAK_TIME_SECONDS * 1000;
 
 
