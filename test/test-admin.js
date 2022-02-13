c5e27eb5738317b57748636eec87fa4b87bda151
Gernot H. Reichel <gernot.reichel@me.com>
2022-02-13 18:51:53 +0100

    fixed error with rmdirSync

--- test/test-admin.js
+++ test/test-admin.js
@@ -14,42 +14,23 @@ const assert = chai.assert;
 require('moment-timezone');
 
 /** ************************************************************ */
-const rmDir = function (dirPath, removeSelf) {
-  let files;
-  let bRemoveSelf = removeSelf;
-  if (removeSelf === undefined) {
-    bRemoveSelf = true;
-  }
-
-  try {
-    files = fs.readdirSync(dirPath);
-  } catch (e) {
-    return;
-  }
-
-  if (files.length > 0) {
-    for (let i = 0; i < files.length; i++) {
-      const filePath = `${dirPath}/${files[i]}`;
-      if (fs.statSync(filePath).isFile()) fs.unlinkSync(filePath);
-      else rmDir(filePath);
-    }
-  }
-  if (bRemoveSelf) fs.rmdirSync(dirPath);
-};
-/** ************************************************************ */
 
 
 describe('test util.dumpModels', () => {
   before(() => {
     db = require('../db');
-    rmdirSync('./dump');
+    fs.rmSync('./dump', { recursive: true, force: true }, err => { console.log(err) });
   });
 
-  it('dumping the Model Toggle; should lead to a new file in /dump', async () => {
+  after(() => {
+    fs.rmSync('./dump', { recursive: true, force: true }, err => { console.log(err) });
+  });
+
+  it.only('dumping the Model Toggle; should lead to a new file in /dump', async () => {
     try {
       //const result = await util.dumpModel(mongoose.model('Toggle'));
       const result = await util.dumpModels();
-      console.log(result)
+      //console.log(result)
       expect(result).to.be.an('array');
       expect(result.length).to.gt(0);
       expect(result[0]).to.have.property('size');
@@ -57,13 +38,13 @@ describe('test util.dumpModels', () => {
 
       const data = fs.readFileSync(result[0].filename);
       expect(data).to.not.be.empty;
-      rmDir('./dump');
     } catch (error) {
       console.log(error);
       assert.fail('should not throw exception')
     }
   }).timeout(20000);
 });
+
 describe('test util.backupTimeEnties', () => {
   it.skip('backing up the database', async () => {
     try {
