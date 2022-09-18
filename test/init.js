355f4071d9562db698e00feeba04b8692a2fb6f1
Gernot H. Reichel <gernot.reichel@me.com>
2022-09-18 14:38:16 +0200

    corrected DB url construction for test cases

--- test/init.js
+++ test/init.js
@@ -1,4 +1,8 @@
 require('dotenv').config();
 
 process.env.SLACK_URL = '';
-process.env.MONGODB_URL = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URI}`;
+if(process.env.MONGODB_USER && process.env.MONGODB_PASSWORD && process.env.MONGODB_URI) {
+  process.env.MONGODB_URL = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URI}`;
+} else {
+  process.env.MONGODB_URL = 'mongodb+srv://timetracker-user:cyfgeq-mypnu9-vozFyv@nobiocluster.arj0i.mongodb.net/timetrack?retryWrites=true&w=majority';
+}
