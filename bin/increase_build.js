5bf7250d921f28ec6a60f6edf9f5e54046919841
Gernot H. Reichel <gernot.reichel@me.com>
2022-11-13 10:34:44 +0100

    updated dependencies

--- bin/increase_build.js
+++ bin/increase_build.js
@@ -12,7 +12,7 @@ const { exec } = require('child_process');
 const package_json = require('../package.json');
 
 const arg = process.argv.slice(2);
-if (arg === '--help') {
+if (arg == '--help') {
   console.log('Usage: node bin/build.js [<ma|mi|b>]');
   process.exit(1);
 }
@@ -22,18 +22,18 @@ const versions = package_json.version.split('.');
 let major_version = parseInt(versions[0]);
 let minor_version = parseInt(versions[1]);
 let build_version = parseInt(versions[2]);
-if (arg === 'ma') {
+if (arg == 'ma') {
   major_version++;
   minor_version = 0;
   build_version = 0;
   modified = true;
 }
-if (arg === 'mi') {
+if (arg == 'mi') {
   minor_version++;
   build_version = 0;
   modified = true;
 }
-if (!arg || arg === '' || arg === 'b') {
+if (!arg || arg == '' || arg == 'b') {
   build_version++;
   modified = true;
 }
