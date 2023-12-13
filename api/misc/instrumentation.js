650515773688d374c00c4cbe4ef8cbdc9ee69f1e
Gernot H. Reichel <gernot.reichel@me.com>
2023-12-13 11:27:18 +0100

    #130 goldplating #2

--- api/misc/instrumentation.js
+++ api/misc/instrumentation.js
@@ -1,14 +1,13 @@
 require('dotenv').config();
+
+console.log(`OTEL instrumentation using trace server  ${process.env.OTEL_TRACE_URL}`);
+console.log(`OTEL instrumentation using metric server ${process.env.OTEL_METRICS_URL}`);
+console.log(`OTEL service nam is '${process.env.OTEL_SERVICE_NAME}'`);
+
 const opentelemetry = require('@opentelemetry/sdk-node');
-const {
-  getNodeAutoInstrumentations,
-} = require('@opentelemetry/auto-instrumentations-node');
-const {
-  OTLPTraceExporter,
-} = require('@opentelemetry/exporter-trace-otlp-proto');
-const {
-  OTLPMetricExporter,
-} = require('@opentelemetry/exporter-metrics-otlp-proto');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-proto');
+const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-proto');
 const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
 
 const sdk = new opentelemetry.NodeSDK({
@@ -21,7 +20,6 @@ const sdk = new opentelemetry.NodeSDK({
   metricReader: new PeriodicExportingMetricReader({
     exporter: new OTLPMetricExporter({
       // url: '<your-otlp-endpoint>/v1/metrics', // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
-      // url: process.env.OTEL_TRACE_URL,
       url: process.env.OTEL_METRICS_URL,
       headers: {}, // an optional object containing custom headers to be sent with each request
       concurrencyLimit: 1, // an optional limit on pending requests
@@ -29,9 +27,5 @@ const sdk = new opentelemetry.NodeSDK({
   }),
   instrumentations: [getNodeAutoInstrumentations()],
 });
-
-console.log(`OTEL instrumentation using trace server  ${process.env.OTEL_TRACE_URL}`);
-console.log(`OTEL instrumentation using metric server ${process.env.OTEL_METRICS_URL}`);
-console.log(`OTEL service nam is ${process.env.OTEL_SERVICE_NAME}`);
 
 sdk.start();
