e3694c16980870130c828dd47898ab7e603eb8c1
Gernot H. Reichel <gernot.reichel@me.com>
2023-12-14 15:50:25 +0100

    #130 using default url http://localhost:4318/v1/

--- api/misc/instrumentation.js
+++ api/misc/instrumentation.js
@@ -13,14 +13,14 @@ const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
 const sdk = new opentelemetry.NodeSDK({
   traceExporter: new OTLPTraceExporter({
     // optional - default url is http://localhost:4318/v1/traces
-    url: process.env.OTEL_TRACE_URL,
+    // url: process.env.OTEL_TRACE_URL,
     // optional - collection of custom headers to be sent with each request, empty by default
     headers: {},
   }),
   metricReader: new PeriodicExportingMetricReader({
     exporter: new OTLPMetricExporter({
       // url: '<your-otlp-endpoint>/v1/metrics', // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
-      url: process.env.OTEL_METRICS_URL,
+      // url: process.env.OTEL_METRICS_URL,
       headers: {}, // an optional object containing custom headers to be sent with each request
       concurrencyLimit: 1, // an optional limit on pending requests
     }),
