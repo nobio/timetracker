6c533fe89eeb05b48de8c9858a4b60a0987e358d
Gernot H. Reichel <gernot.reichel@me.com>
2024-01-25 09:15:56 +0100

    eyJhbGciOiJSUzI1NiIsImtpZCI6IkMwZDJ2UnNvZEE0TWpWblZPbERyanhSREtCak5pamVTaHlRV19lRUJaX28ifQ.eyJhdWQiOlsiaHR0cHM6Ly9rdWJlcm5ldGVzLmRlZmF1bHQuc3ZjLmNsdXN0ZXIubG9jYWwiLCJrM3MiXSwiZXhwIjoxNzA2MTA0Njc0LCJpYXQiOjE3MDYxMDEwNzQsImlzcyI6Imh0dHBzOi8va3ViZXJuZXRlcy5kZWZhdWx0LnN2Yy5jbHVzdGVyLmxvY2FsIiwia3ViZXJuZXRlcy5pbyI6eyJuYW1lc3BhY2UiOiJrdWJlcm5ldGVzLWRhc2hib2FyZCIsInNlcnZpY2VhY2NvdW50Ijp7Im5hbWUiOiJhZG1pbi11c2VyIiwidWlkIjoiYjFjZmQxOTQtYTdmMS00ZWNkLThkNzktM2Y4ZjMzNmI4NjY0In19LCJuYmYiOjE3MDYxMDEwNzQsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDprdWJlcm5ldGVzLWRhc2hib2FyZDphZG1pbi11c2VyIn0.DJpy9-wW2E8bGZl4ktoWzVlj6de0i31GkvMWfT-8NL2irMv8GAlQwuuCnEN3ZneD-iRnW3oXJekrkpGXyJJlBfGrVB0h8tnY1LpdXRYtiPWKvmsCV43fH81BqH7URWIKYz96edy3vOYck7CKUyBPLK6vnCrY25oZVzkg0ZAhvQhC_3OLdugigbWYKS-QOaa2ak5BQ22mvOFfIUrJ01DylnMsbNZySvTvZK_Kil3-EgUFz-1IlzVVagqxL6zTXQM4wfPu4kT8T7db2iX3jzxLRCAqg3Ry_jHgqJ6-dm9o25OnpAqHxuoKsAaEZdcRB06Rj063i_gRYKBGCJi0QwOy7g

--- k8s/merge.sh
+++ k8s/merge.sh
@@ -2,7 +2,9 @@ TARGET_YAML='app.yaml'
 echo ${TARGET_YAML}
 rm -f $TARGET_YAML
 touch $TARGET_YAML
-cat timetracker-namespace.yaml > $TARGET_YAML
+cat admin-user.yaml > $TARGET_YAML
+echo '---' >> $TARGET_YAML
+cat timetracker-namespace.yaml >> $TARGET_YAML
 echo '---' >> $TARGET_YAML
 for f in ./*/*.yaml; do
   cat $f >> $TARGET_YAML
