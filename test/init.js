0700632fd411e74ae33597aa1b184b29531ae49b
Gernot H. Reichel <gernot.reichel@me.com>
2024-03-28 09:08:41 +0100

    disabled esling max-len

--- test/init.js
+++ test/init.js
@@ -1,10 +1,11 @@
+/* eslint-disable max-len */
 require('dotenv').config();
 
 process.env.SLACK_URL = '';
 if (process.env.MONGODB_PROTOCOL
   && process.env.MONGODB_USER
-   && process.env.MONGODB_PASSWORD
-   && process.env.MONGODB_URI) {
+  && process.env.MONGODB_PASSWORD
+  && process.env.MONGODB_URI) {
   process.env.MONGODB_URL = `${process.env.MONGODB_PROTOCOL}://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URI}`;
 } else {
   process.env.MONGODB_URL = 'mongodb+srv://timetracker-user:cyfgeq-mypnu9-vozFyv@nobiocluster.arj0i.mongodb.net/timetrack?retryWrites=true&w=majority';
